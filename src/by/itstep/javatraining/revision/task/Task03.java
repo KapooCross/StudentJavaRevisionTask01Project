d2dda3a762ec6ecb7e25caa6fd0395077c9f5d3b
JB <87502853+KapooCross@users.noreply.github.com>
2023-03-25 21:01:37 +0300

    Update Task03.java ezResh

--- src/by/itstep/javatraining/revision/task/Task03.java
+++ src/by/itstep/javatraining/revision/task/Task03.java
@@ -26,6 +26,7 @@ package by.itstep.javatraining.revision.task;
 
 public class Task03 {
     public static int task03(int number) {
+    // return (number > 9 || number < 0) ? return Math.abs((number / 10) % 10)  :  return 0;        
         if (number > 9 || number < 0) {
             return Math.abs((number / 10) % 10);
         }
